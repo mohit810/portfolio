1b3af8b018107a067f8ee934d2bacfe2922d20d0
Gh0sT <iammohit810@gmail.com>
2024-03-02 14:00:51 -0800

    animation correction

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,23 @@
+"use client"; // This is a client component 👈🏽
 import "./header.css";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const first_text = "Mohit Yadav";
+  const [text1, setText1] = useState("");
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setText1(first_text.slice(0, text1.length + 1));
+    }, 100);
+    return () => clearTimeout(timeout);
+  }, [text1]);
+
   return (
     <nav className="w-full h-20 p-2 shadow-lg bg-white">
       <div className="w-full h-full flex flex-row my-auto">
         <div className="w-14 h-14 my-auto mx-10 bg-yellow-300 rounded-full" />
-        <h1 className="px-2 my-auto relative w-[max-content] font-mono text-2xl before:absolute before:inset-0 before:animate-typewriter before:bg-white after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-black">
-          Mohit Yadav
-        </h1>
+        <h1 className="my-auto text-black text-2xl blinking-cursor">{text1}</h1>
       </div>
     </nav>
   );
